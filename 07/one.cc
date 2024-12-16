ef134076989b62bd85e6db434d1687930fbfc2a8
Jesper Kristiansson <jeppe.00@live.se>
2024-12-16 23:12:40 +0100

    Day 7

--- 07/one.cc
+++ 07/one.cc
@@ -15,6 +15,72 @@
 
 using namespace std;
 
+struct Equation{
+    long target;
+    vector<long> operands;
+};
+
+Equation getEquation(const string &line){
+    Equation eq;
+
+    stringstream ss(line);
+    ss >> eq.target;
+    ss.ignore();
+    while(ss){
+        long num;
+        ss >> num;
+
+        if(!ss){
+            break;
+        }
+
+        eq.operands.push_back(num);
+    }
+
+    return eq;
+}
+
+vector<Equation> getEquations(const vector<string> &lines){
+    vector<Equation> eqs;
+    for(const auto &line : lines){
+        eqs.push_back(getEquation(line));
+    }
+    return eqs;
+}
+
+bool helper(const Equation &eq, size_t idx, long ack){
+    if(idx >= eq.operands.size()){
+        return ack == eq.target;
+    }
+
+    long sum_add = ack + eq.operands[idx];
+    if(helper(eq, idx+1, sum_add)){
+        return true;
+    }
+    long sum_mul = ack * eq.operands[idx];
+    if(helper(eq, idx+1, sum_mul)){
+        return true;
+    }
+
+    return false;
+}
+
+bool verifyEquation(const Equation &eq){
+    return helper(eq, 1, eq.operands[0]);
+}
+
+long sumEquations(const vector<Equation> &eqs){
+    long sum = 0;
+    for(const auto &eq : eqs){
+        if(verifyEquation(eq)){
+            sum += eq.target;
+        }
+    }
+    return sum;
+}
+
 int main(){
-    
+    vector<string> lines = readLines();
+    vector<Equation> eqs = getEquations(lines);
+    cout << sumEquations(eqs);
 }
--- 07/two.cc
+++ 07/two.cc
@@ -15,6 +15,91 @@
 
 using namespace std;
 
+struct Equation{
+    long target;
+    vector<long> operands;
+};
+
+Equation getEquation(const string &line){
+    Equation eq;
+
+    stringstream ss(line);
+    ss >> eq.target;
+    ss.ignore();
+    while(ss){
+        long num;
+        ss >> num;
+
+        if(!ss){
+            break;
+        }
+
+        eq.operands.push_back(num);
+    }
+
+    return eq;
+}
+
+vector<Equation> getEquations(const vector<string> &lines){
+    vector<Equation> eqs;
+    for(const auto &line : lines){
+        eqs.push_back(getEquation(line));
+    }
+    return eqs;
+}
+
+int countDigits(long num){
+    int numDigits = 0;
+    while(num != 0){
+        num /= 10;
+        numDigits++;
+    }
+    return numDigits;
+}
+
+bool helper(const Equation &eq, size_t idx, long ack){
+    if(idx >= eq.operands.size()){
+        return ack == eq.target;
+    }
+
+    long sum_add = ack + eq.operands[idx];
+    if(helper(eq, idx+1, sum_add)){
+        return true;
+    }
+    long sum_mul = ack * eq.operands[idx];
+    if(helper(eq, idx+1, sum_mul)){
+        return true;
+    }
+
+    int numDigits = countDigits(eq.operands[idx]);
+    long sum_cat = ack;
+    for(int i = 0; i < numDigits; i++){
+        sum_cat *= 10;
+    }
+    sum_cat += eq.operands[idx];
+    if(helper(eq, idx+1, sum_cat)){
+        return true;
+    }
+
+    return false;
+}
+
+bool verifyEquation(const Equation &eq){
+    return helper(eq, 1, eq.operands[0]);
+}
+
+long sumEquations(const vector<Equation> &eqs){
+    long sum = 0;
+    for(const auto &eq : eqs){
+        if(verifyEquation(eq)){
+            sum += eq.target;
+        }
+    }
+    return sum;
+}
+
 int main(){
-    
+    vector<string> lines = readLines();
+    vector<Equation> eqs = getEquations(lines);
+    cout << sumEquations(eqs);
 }
