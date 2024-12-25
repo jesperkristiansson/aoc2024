5c939944ff822b2a0ce54779418443e56bf8ff8d
Jesper Kristiansson <jeppe.00@live.se>
2024-12-25 11:19:12 +0100

    day 22

--- 22/one.cc
+++ 22/one.cc
@@ -15,6 +15,24 @@
 
 using namespace std;
 
+long long advance(long long secret, int n){
+    for(int i = 0; i < n; i++){
+        secret = (secret ^ secret*64) % 16777216;
+        secret = (secret ^ secret/32) % 16777216;
+        secret = (secret ^ secret*2048) % 16777216;
+    }
+    return secret;
+}
+
+long long score(const vector<long long> &secrets){
+    long long sum = 0;
+    for(long long secret : secrets){
+        sum += advance(secret, 2000);
+    }
+    return sum;
+}
+
 int main(){
-    
+    vector<long long> secrets = readInput_vec<long long>();
+    cout << score(secrets) << endl;
 }
--- 22/two.cc
+++ 22/two.cc
@@ -15,6 +15,97 @@
 
 using namespace std;
 
+using Pattern = array<int8_t, 4>;
+
+vector<uint8_t> getPriceList(long long secret, int n){
+    vector<uint8_t> priceList;
+    priceList.reserve(n+1);
+    priceList.push_back(secret % 10);
+    for(int i = 0; i < n; i++){
+        secret = (secret ^ secret << 6) & 0xFFFFFF;
+        secret = (secret ^ secret >> 5) & 0xFFFFFF;
+        secret = (secret ^ secret << 11) & 0xFFFFFF;
+        priceList.push_back(secret % 10);
+    }
+    return priceList;
+}
+
+vector<int8_t> getPriceDiff(const vector<uint8_t> &prices){
+    vector<int8_t> priceDiff;
+    priceDiff.reserve(prices.size());
+
+    uint8_t lastPrice = prices[0];
+    for(uint8_t price : prices){
+        priceDiff.push_back(price - lastPrice);
+        lastPrice = price;
+    }
+
+    return priceDiff;
+}
+
+map<Pattern, uint8_t> getPatternMap(const vector<int8_t> &priceDiff, const vector<uint8_t> &priceList){
+    map<Pattern, uint8_t> patternMap;
+
+    for(size_t i = 3; i < priceDiff.size(); i++){
+        Pattern pattern;
+        pattern[0] = priceDiff[i-3];
+        pattern[1] = priceDiff[i-2];
+        pattern[2] = priceDiff[i-1];
+        pattern[3] = priceDiff[i];
+
+        patternMap.emplace(pattern, priceList[i]);
+    }
+
+    return patternMap;
+}
+
+long long score(const vector<long long> &secrets){
+    vector<vector<uint8_t>> priceLists;
+    priceLists.reserve(secrets.size());
+
+    vector<vector<int8_t>> priceDiffs;
+    priceDiffs.reserve(secrets.size());
+    for(long long secret : secrets){
+        priceLists.push_back(getPriceList(secret, 2000));
+        priceDiffs.push_back(getPriceDiff(priceLists.back()));
+    }
+
+    vector<map<Pattern, uint8_t>> patternMaps;
+    for(size_t i = 0; i < priceDiffs.size(); i++){
+        patternMaps.push_back(getPatternMap(priceDiffs[i], priceLists[i]));
+    }
+
+    int max = 0;
+    Pattern pattern;
+    for(int8_t i1 = -9; i1 <= 9; i1++){
+        pattern[0] = i1;
+        for(int8_t i2 = -9; i2 <= 9; i2++){
+            pattern[1] = i2;
+            for(int8_t i3 = -9; i3 <= 9; i3++){
+                pattern[2] = i3;
+                for(int8_t i4 = -9; i4 <= 9; i4++){
+                    pattern[3] = i4;
+
+                    int sum = 0;
+                    for(const map<Pattern, uint8_t> &patternMap : patternMaps){
+                        auto it = patternMap.find(pattern);
+                        if(it != patternMap.end()){
+                            sum += it->second;
+                        }
+                    }
+
+                    if(sum > max){
+                        max = sum;
+                    }
+                }
+            }
+        }
+    }
+
+    return max;
+}
+
 int main(){
-    
+    vector<long long> secrets = readInput_vec<long long>();
+    cout << score(secrets) << endl;
 }
