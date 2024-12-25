1ec5502422ad5efd9a2220c6b1f0f37a7fe4e36d
Jesper Kristiansson <jeppe.00@live.se>
2024-12-25 22:02:13 +0100

    day 19

--- 19/one.cc
+++ 19/one.cc
@@ -15,6 +15,81 @@
 
 using namespace std;
 
+vector<string> getAvailablePatterns(const string &line){
+    vector<string> availablePatterns;
+
+    istringstream ss(line);
+    while(!ss.eof()){
+        string token;
+        ss >> token;
+
+        if(token.back() == ','){
+            token.pop_back();
+        }
+
+        availablePatterns.push_back(token);
+    }
+
+    return availablePatterns;
+}
+
+vector<string> getTowels(vector<string>::const_iterator begin, vector<string>::const_iterator end){
+    vector<string> towels;
+
+    while(begin != end){
+        towels.push_back(*begin);
+        ++begin;
+    }
+
+    return towels;
+}
+
+bool isSubstrOf(const string &substr, const string &s, size_t s_i){
+    size_t sizeLeft = s.size() - s_i;
+    if(substr.size() > sizeLeft){
+        return false;
+    }
+
+    for(size_t i = 0; i < substr.size(); i++){
+        if(substr[i] != s[s_i + i]){
+            return false;
+        }
+    }
+    return true;
+}
+
+bool isPossible(const vector<string> &patterns, const string &towel, size_t towel_i){
+    if(towel_i >= towel.size()){
+        return true;
+    }
+
+    for(const string &pattern : patterns){
+        if(!isSubstrOf(pattern, towel, towel_i)){
+            continue;
+        }
+
+        if(isPossible(patterns, towel, towel_i + pattern.size())){
+            return true;
+        }
+    }
+    return false;
+}
+
+int countPossible(const vector<string> &patterns, const vector<string> &towels){
+    int possible = 0;
+    for(const string &towel : towels){
+        if(isPossible(patterns, towel, 0)){
+            ++possible;
+        }
+    }
+
+    return possible;
+}
+
 int main(){
-    
+    vector<string> lines = readLines();
+    vector<string> availablePatterns = getAvailablePatterns(lines[0]);
+    vector<string> towels = getTowels(lines.begin() + 2, lines.end());
+
+    cout << countPossible(availablePatterns, towels) << endl;
 }
--- 19/two.cc
+++ 19/two.cc
@@ -6,6 +6,7 @@
 #include <vector>
 #include <set>
 #include <map>
+#include <unordered_map>
 #include <limits>
 #include <utility>
 #include <cctype>
@@ -15,6 +16,86 @@
 
 using namespace std;
 
+vector<string> getAvailablePatterns(const string &line){
+    vector<string> availablePatterns;
+
+    istringstream ss(line);
+    while(!ss.eof()){
+        string token;
+        ss >> token;
+
+        if(token.back() == ','){
+            token.pop_back();
+        }
+
+        availablePatterns.push_back(token);
+    }
+
+    return availablePatterns;
+}
+
+vector<string> getTowels(vector<string>::const_iterator begin, vector<string>::const_iterator end){
+    vector<string> towels;
+
+    while(begin != end){
+        towels.push_back(*begin);
+        ++begin;
+    }
+
+    return towels;
+}
+
+bool isSubstrOf(const string &substr, const string &s, size_t s_i){
+    size_t sizeLeft = s.size() - s_i;
+    if(substr.size() > sizeLeft){
+        return false;
+    }
+
+    for(size_t i = 0; i < substr.size(); i++){
+        if(substr[i] != s[s_i + i]){
+            return false;
+        }
+    }
+    return true;
+}
+
+long long countPossible2(const vector<string> &patterns, const string &towel, size_t towel_i, unordered_map<size_t, long long> &cache){
+    if(towel_i >= towel.size()){
+        return 1;
+    }
+
+    auto it = cache.find(towel_i);
+    if(it != cache.end()){
+        return it->second;
+    }
+
+    long long count = 0;
+    for(const string &pattern : patterns){
+        if(!isSubstrOf(pattern, towel, towel_i)){
+            continue;
+        }
+
+        count += countPossible2(patterns, towel, towel_i + pattern.size(), cache);
+    }
+
+    cache[towel_i] = count;
+    return count;
+}
+
+long long countPossible(const vector<string> &patterns, const vector<string> &towels){
+    long long possible = 0;
+    for(const string &towel : towels){
+        unordered_map<size_t, long long> cache;
+        possible += countPossible2(patterns, towel, 0, cache);
+    }
+
+    return possible;
+}
+
 int main(){
-    
+    vector<string> lines = readLines();
+    vector<string> availablePatterns = getAvailablePatterns(lines[0]);
+    vector<string> towels = getTowels(lines.begin() + 2, lines.end());
+
+    cout << countPossible(availablePatterns, towels) << endl;
 }
