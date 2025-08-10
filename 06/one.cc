dcdfacd18892a2eb2873e0eae3aa10a1397cb860
Jesper Kristiansson <jeppe.00@live.se>
2025-08-10 15:59:35 +0200

    day 06: update part 1 solution

--- 06/one.cc
+++ 06/one.cc
@@ -12,106 +12,45 @@
 #include <cstring>
 
 #include "readInput.hpp"
+#include "Grid.hpp"
 
 using namespace std;
 
-using matrix = vector<string>;
-
-struct Position{
-    int r,c;
-};
-
-struct Direction{
-    int dr, dc;
-};
-
-bool isInside(Position pos, size_t ROWS, size_t COLS){
-    return pos.r >= 0 && pos.r < (int)ROWS && pos.c >= 0 && pos.c < (int)COLS;
-}
-
-Position nextPos(Position current, Direction dir){
-    Position next;
-    next.r = current.r + dir.dr;
-    next.c = current.c + dir.dc;
-    return next;
-}
-
-Direction turnRight(Direction dir){
-    Direction newDir;
-    if(dir.dr == 0 && dir.dc == 1){     //right
-        newDir.dr = 1;
-        newDir.dc = 0;
-    } else if(dir.dr == 0 && dir.dc == -1){     //left
-        newDir.dr = -1;
-        newDir.dc = 0;
-    } else if(dir.dr == 1 && dir.dc == 0){     //down
-        newDir.dr = 0;
-        newDir.dc = -1;
-    } else if(dir.dr == -1 && dir.dc == 0){     //up
-        newDir.dr = 0;
-        newDir.dc = 1;
-    } else{
-        //error
-        newDir.dr = 0;
-        newDir.dc = 0;
-        cout << "Error: unknown direction" << endl;
-    }
-
-    return newDir;
-}
-
-char &symbolAt(matrix &mat, Position pos){
-    return mat[pos.r][pos.c];
-}
-
-Position getStartPosition(const matrix &mat, size_t ROWS, size_t COLS){
-    for(size_t r = 0; r < ROWS; r++){
-        for(size_t c = 0; c < COLS; c++){
-            if(mat[r][c] == '^'){
-                Position pos;
-                pos.r = (int)r;
-                pos.c = (int)c;
-                return pos;
+Point getStartPoint(const Grid<char> &grid){
+    for(int y = 0; y < grid.yMax; y++){
+        for(int x = 0; x < grid.xMax; x++){
+            Point p(x, y);
+            if(grid.at(p) == '^'){
+                return p;
             }
         }
     }
-}
-
-Direction getStartDirection(){
-    Direction dir;
-    dir.dr = -1;
-    dir.dc = 0;
-    return dir;
+    return Point(-1, -1);
 }
 
 int main(){
-    matrix mat = readLines();
+    vector<string> mat = readLines();
+    Grid<char> grid(mat);
 
-    const size_t ROWS = mat.size();
-    const size_t COLS = mat[0].size();
-
-    cout << ROWS << " " << COLS << endl;
-
-    Position pos = getStartPosition(mat, ROWS, COLS);
-    Direction dir = getStartDirection();
+    Point pos = getStartPoint(grid);
+    Direction dir = NORTH;
 
     int cnt = 0;
-    while(isInside(pos, ROWS, COLS)){
-        if(symbolAt(mat, pos) != 'X'){
+    while(grid.contains(pos)){
+        if(grid.at(pos) != 'X'){
             ++cnt;
-            symbolAt(mat, pos) = 'X';
+            grid.at(pos) = 'X';
         }
-        Position next = nextPos(pos, dir);
-        if(!isInside(next, ROWS, COLS)){
+        Point next = pos + dir;
+        if(!grid.contains(next)){
             break;
         }
 
-        if(symbolAt(mat, next) == '#'){     //turn right
-            dir = turnRight(dir);
+        if(grid.at(next) == '#'){     //turn right
+            dir = dir.rotateRight();
         } else{
             pos = next;
         }
-
     }
 
     cout << cnt << endl;
