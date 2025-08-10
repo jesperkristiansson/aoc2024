64f659576b8f1ebeeb73672d12785f07a88b18e2
Jesper Kristiansson <jeppe.00@live.se>
2025-08-10 16:17:38 +0200

    Day 6 part 2

--- 06/two.cc
+++ 06/two.cc
@@ -12,9 +12,72 @@
 #include <cstring>
 
 #include "readInput.hpp"
+#include "Grid.hpp"
 
 using namespace std;
 
+Point getStartPoint(const Grid<char> &grid){
+    for(int y = 0; y < grid.yMax; y++){
+        for(int x = 0; x < grid.xMax; x++){
+            Point p(x, y);
+            if(grid.at(p) == '^'){
+                return p;
+            }
+        }
+    }
+    return Point(-1, -1);
+}
+
+bool checkLoop(const Grid<char> &grid, Point pos, Direction dir){
+    std::map<Point, set<Direction>> steps;
+    while(grid.contains(pos)){
+        if(steps[pos].contains(dir)){
+            return true;
+        }
+        steps[pos].insert(dir);
+        Point next = pos + dir;
+        if(!grid.contains(next)){
+            break;
+        }
+
+        if(grid.at(next) == '#'){     //turn right
+            dir = dir.rotateRight();
+        } else{
+            pos = next;
+        }
+    }
+
+    return false;
+}
+
 int main(){
-    
+    vector<string> mat = readLines();
+    Grid<char> grid(mat);
+    Grid<unsigned char> tried(grid.xMax, grid.yMax);
+
+    Point pos = getStartPoint(grid);
+    Direction dir = NORTH;
+
+    int cnt = 0;
+    while(grid.contains(pos)){
+        Point next = pos + dir;
+        if(!grid.contains(next)){
+            break;
+        }
+
+        if(grid.at(next) == '#'){     //turn right
+            dir = dir.rotateRight();
+        } else{
+            if(!tried.at(next)){
+                tried.at(next) = true;
+                grid.at(next) = '#';
+                cnt += checkLoop(grid, pos, dir);
+                grid.at(next) = '.';
+            }
+
+            pos = next;
+        }
+    }
+
+    cout << cnt << endl;
 }
