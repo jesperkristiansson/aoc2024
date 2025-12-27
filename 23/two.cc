25ef97be4ccbb92a0bd1c169b18c3ec4381a63ad
Jesper Kristiansson <jeppe.00@live.se>
2025-12-27 03:30:03 +0100

    day 23: part 2

--- 23/two.cc
+++ 23/two.cc
@@ -11,6 +11,7 @@
 #include <cctype>
 #include <cstring>
 #include <cassert>
+#include <tuple>
 
 #include "readInput.hpp"
 
@@ -23,7 +24,6 @@ struct Connection{
 struct Computer{
     string name;
     set<const Computer *> connections;
-    bool visited{false};
 };
 
 istream &operator>>(istream &is, Connection &c){
@@ -32,52 +32,51 @@ istream &operator>>(istream &is, Connection &c){
     return is;
 }
 
-bool isConnectedToClique(const Computer *computer, const vector<Computer *> &clique){
-    for(const Computer *computer2: clique){
-        if(!computer->connections.contains(computer2)){
-            return false;
-        }
-    }
-    return true;
-}
-
-bool getCliqueHelper(const vector<Computer *> &computers, size_t size, vector<Computer *> &current){
-    if(computers.size() == size){
-        return true;
-    }
-
-    for(Computer *computer : computers){
-        if(computer->visited || !isConnectedToClique(computer, current)){
-            continue;
-        }
-        current.push_back(computer);
-        computer->visited = true;
-        if(getCliqueHelper(computers, size, current)){
-            return true;
-        }
-        computer->visited = false;
-        current.pop_back();
-    }
-    return false;
-}
-
-
-vector<Computer *> getClique(const vector<Computer *> &computers, size_t size, bool &success){
-    for(auto *computer : computers){
-        computer->visited = false;
-    }
-    vector<Computer *> clique;
-    success = getCliqueHelper(computers, size, clique);
-    return clique;
-}
-
-string getPassword(vector<Computer *> clique){
+string getPassword(vector<const Computer *> clique){
     string password;
-    std::sort(clique.begin(), clique.end(), [](const Computer *comp1, const Computer *comp2){return comp1->name > comp2->name;});
+    std::sort(clique.begin(), clique.end(), [](const Computer *comp1, const Computer *comp2){return comp1->name < comp2->name;});
     for(auto *comp : clique){
         password += comp->name;
+        password += ',';
     }
+    password.pop_back();
     return password;
+}
+
+vector<const Computer *> findClique(vector<const Computer *> computers, size_t size){
+    if(computers.size() < size){
+        return {};
+    }
+    if(computers.size() == size){
+        for(size_t i = 0; i < computers.size(); i++){
+            const Computer *computerToCheck = computers[i];
+            for(size_t j = 0; j < computers.size(); j++){
+                if(i == j){
+                    continue;
+                }
+                if(!computerToCheck->connections.contains(computers[j])){
+                    return {};
+                }
+            }
+        }
+        return computers;
+    }
+
+    // computers.size() > size
+    vector<const Computer *> newComputers = computers;
+    newComputers.pop_back();
+    for(int i = (int) computers.size()-1; i >= 0; i--){
+        auto res = findClique(newComputers, size);
+        if(!res.empty()){
+            return res;
+        }
+
+        if(i > 0){
+            newComputers[i-1] = computers[i];
+        }
+    }
+
+    return {};
 }
 
 int main(){
@@ -95,45 +94,45 @@ int main(){
         com2.connections.insert(&com1);
     }
 
-    std::vector<std::vector<Computer *>> connectionSets(computers.size());
-    for(auto &[name, computer] : computers){
-        connectionSets[computer.connections.size()].push_back(&computer);
-    }
-    size_t cnt = 0;
-    std::vector<Computer *> availableComputers;
-    for(int i = connectionSets.size() - 1; i >= 0; i--){
-        auto &newComputers = connectionSets[i];
-        cnt += newComputers.size();
-        std::copy(newComputers.begin(), newComputers.end(), std::back_inserter(availableComputers));
-        if(cnt > connectionSets[i].size()){
-            bool success;
-            cout << "finding clique of size " << i << " from " << availableComputers.size() << " nodes" << endl;
-            auto clique = getClique(availableComputers, i, success);
-            //assert((clique.size() == (size_t)i) == success);
-            if(success){
-                cout << "Success" << endl;
-                string password = getPassword(clique);
-                cout << password << endl;
+    vector<const Computer *> maxClique;
+    for(auto &kv : computers){
+        auto &comp = kv.second;
+
+        //+1 needed?
+        vector<vector<const Computer*>> ncn(comp.connections.size()+1);
+        for(const auto *neigh : comp.connections){
+            int numCommonNeighbours = 1;
+            for(const auto *neigh2 : neigh->connections){
+                if(comp.connections.contains(neigh2)){
+                    numCommonNeighbours++;
+                }
+            }
+            ncn[numCommonNeighbours].push_back(neigh);
+        }
+
+        size_t cnt = 0;
+        std::vector<const Computer*> availableComputers;
+        for(int i = ncn.size()-1; i >= 0; i--){
+            if(i + 1 <= (int)maxClique.size()){
+                break;
+            }
+
+            auto &newComputers = ncn[i];
+            cnt += newComputers.size();
+            std::copy(newComputers.begin(), newComputers.end(), std::back_inserter(availableComputers));
+            if((int)cnt < i){
+                continue;
+            }
+
+            //check clique
+            auto res = findClique(availableComputers, i);
+            if(!res.empty()){
+                res.push_back(&comp);
+                maxClique = res;
                 break;
             }
         }
     }
 
-    long score = 0;
-    // for(const auto &[name, comp1] : computers){
-    //     for(const auto &comp2 : comp1.connections){
-    //         for(const auto &comp3 : comp2->connections){
-    //             if((comp1.name[0] == 't' || comp2->name[0] == 't' || comp3->name[0] == 't') && comp3->connections.contains(&comp1)){
-    //                 ++score;
-    //             }
-    //         }
-    //     }
-    // }
-    // score /= 6;
-
-    cout << score << endl;
-
-    // for(auto c : connections){
-    //     cout << c.computer1 << '-' << c.computer2 << endl;
-    // }
+    cout << getPassword(maxClique) << endl;
 }
